6de2bbf3648607b31305a98cd26c295f9f6894c3
YSLCoat <torstein.forseth@outlook.com>
2026-01-24 23:54:38 +0100

    More modularization

--- train.py
+++ train.py
@@ -1,5 +1,19 @@
+import random
+import warnings
+import logging
+import os
+import datetime
+
 import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.multiprocessing as mp
 import torch.nn as nn
+import torch.nn.parallel
+import torch.optim
+import torch.utils.data
+from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, SequentialLR
+from torch.utils.data import Subset
 
 from trainer import TrainingManager
 from models.models import create_model
@@ -7,13 +21,101 @@ from utils.configs_parser import (
     TrainingConfig,
     parse_training_configs
 )
+from utils.torch_utils import (
+    configure_multi_gpu_model,
+    configure_training_device,
+    enable_manual_seed,
+    configure_ddp
+)
 
 
+def configure_training(configs):
+    if configs.seed_val is not None:
+        enable_manual_seed(configs.seed_val)
 
-def main(configs: TrainingConfig):
-    raise NotImplementedError
+    ngpus_per_node = configure_ddp(configs)
+
+    if configs.multiprocessing_distributed:
+        configs.world_size = ngpus_per_node * configs.world_size
+        mp.spawn(main, nprocs=ngpus_per_node, configs=(ngpus_per_node, configs))
+    else:
+        main(configs.gpu, ngpus_per_node, configs)
+
+
+def main(gpu, ngpus_per_node, configs):
+    # TODO: Improve implementation of training_id generation.
+    unique_training_id = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    folder_name = f"{configs.arch}_{unique_training_id}"
+    global best_acc1
+    configs.gpu = gpu
+
+    use_accel = not configs.no_accel and torch.accelerator.is_available()
+
+    if use_accel:
+        if configs.gpu is not None:
+            torch.accelerator.set_device_index(configs.gpu)
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = torch.device("cpu")
+
+    if configs.distributed:
+        if configs.dist_url == "env://" and configs.rank == -1:
+            configs.rank = int(os.environ["RANK"])
+        if configs.multiprocessing_distributed:
+            configs.rank = configs.rank * ngpus_per_node + gpu
+        dist.init_process_group(
+            backend=configs.dist_backend,
+            init_method=configs.dist_url,
+            world_size=configs.world_size,
+            rank=configs.rank,
+        )
+
+    logging.info("=> creating model '{}'".format(configs.arch))
+    model = create_model(configs)
+
+    if not use_accel:
+        logging.info("using CPU, this will be slow")
+    else:
+        configure_multi_gpu_model(configs, model, device, ngpus_per_node)
+
+    train_loader, val_loader, train_sampler, _ = build_data_loaders(configs)
+
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    total_steps = len(train_loader) * configs.epochs
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(), configs.lr, weight_decay=configs.weight_decay
+    )
+
+    main_scheduler = CosineAnnealingLR(
+        optimizer, T_max=total_steps - configs.warmup_period, eta_min=1e-6
+    )
+
+    warmup_scheduler = LinearLR(
+        optimizer, start_factor=0.01, total_iters=configs.warmup_period
+    )
+
+    scheduler = SequentialLR(
+        optimizer,
+        schedulers=[warmup_scheduler, main_scheduler],
+        milestones=[configs.warmup_period],
+    )
+
+    metrics_engine = MetricsEngine(use_accel)
+
+    training_manager = TrainingManager()
+
+    if configs.resume:
+        load_checkpoint(configs, device, model, optimizer, scheduler, metrics_engine)
+
+    if configs.evaluate:
+        validate(val_loader, model, criterion, metrics_engine, configs)
+        return
+    
+    training_manager.train()
 
 
 if __name__ == "__main__":
     configs = parse_training_configs()
-    main(configs)
+    configure_training(configs)
---
+++ utils/torch_utils.py
@@ -0,0 +1,87 @@
+import random
+import warnings
+import os
+import logging
+
+import torch
+import torch.backends.cudnn as cudnn
+
+def configure_training_device(configs):
+    use_accel = not configs.no_accel and torch.accelerator.is_available()
+    if use_accel:
+        if configs.gpu is not None:
+            torch.accelerator.set_device_index(configs.gpu)
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = torch.device("cpu")
+
+    return device
+
+
+def configure_multi_gpu_model(configs, model, device, ngpus_per_node):
+    if configs.distributed:
+        if device.type == "cuda":
+            if configs.gpu is not None:
+                torch.cuda.set_device(configs.gpu)
+                model.cuda(device)
+                configs.batch_size = int(configs.batch_size / ngpus_per_node)
+                configs.workers = int((configs.workers + ngpus_per_node - 1) / ngpus_per_node)
+                model = torch.nn.parallel.DistributedDataParallel(
+                    model, device_ids=[configs.gpu]
+                )
+            else:
+                model.cuda()
+                model = torch.nn.parallel.DistributedDataParallel(model)
+    elif device.type == "cuda":
+        if configs.arch.startswith("alexnet") or configs.arch.startswith("vgg"):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+    else:
+        model.to(device)
+
+
+def enable_manual_seed(seed_val):
+    random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    cudnn.deterministic = True
+    cudnn.benchmark = False
+    warnings.warn(
+        "You have chosen to seed training. "
+        "This will turn on the CUDNN deterministic setting, "
+        "which can slow down your training considerably! "
+        "You may see unexpected behavior when restarting "
+        "from checkpoints."
+    )
+
+
+def configure_ddp(configs):
+    if configs.gpu is not None:
+        warnings.warn(
+            "You have chosen a specific GPU. This will completely "
+            "disable data parallelism."
+        )
+
+    if configs.dist_url == "env://" and configs.world_size == -1:
+        configs.world_size = int(os.environ["WORLD_SIZE"])
+
+    configs.distributed = configs.world_size > 1 or configs.multiprocessing_distributed
+
+    use_accel = not configs.no_accel and torch.accelerator.is_available()
+
+    if use_accel:
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = torch.device("cpu")
+
+    logging.info(f"Using device: {device}")
+
+    if device.type == "cuda":
+        ngpus_per_node = torch.accelerator.device_count()
+        if ngpus_per_node == 1 and configs.dist_backend == "nccl":
+            warnings.warn(
+                "nccl backend >=2.5 requires GPU count>1, see https://github.com/NVIDIA/nccl/issues/103 perhaps use 'gloo'"
+            )
+    else:
+        ngpus_per_node = 1
