fb80ebc478a3b0ce0e7166daf1dde81673e1da4a
YSLCoat <torstein.forseth@outlook.com>
2026-03-01 20:39:36 +0100

    Memory usage explode, we likely wanna decouple msa head dim to make this cheaper

--- training_config_templates/imagenet_train.sh
+++ training_config_templates/imagenet_train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 n_epochs=90
 batch_size=256
-model_architecture="FeedForwardNeuralNetwork"
+model_architecture="VisionTransformer"
 dataset="ImageNet"
 model_save_dir="/home/yslcoat/trained_models"
 data_dir="/home/yslcoat/data/imagenet1k/"
@@ -10,7 +10,5 @@ systemd-inhibit --what=sleep python ../train.py \
     --dataset "$dataset" \
     --imagenet-root "$data_dir" \
     --arch "$model_architecture" \
-    --ff-output-dim 1000 \
-    --ff-input-size 150528 \
     --epochs "$n_epochs" \
     --batch-size "$batch_size" \
